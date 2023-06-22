0279846a5630b8b2db6afe7d6a224a67368f7fe6
msmanawadu <msmanawadu@gmail.com>
2023-06-22 15:30:13 +0530

    Implement PUT endpoint to return updated document

--- src/server.js
+++ src/server.js
@@ -54,7 +54,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    res.send(`The ${name} article now has ${article.upvotes} upvotes !!!`);
+    res.json(article);
   } else {
     res.send('The article does not exist');
   }
