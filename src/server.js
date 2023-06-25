e612a8fbd4223ee61bc1b6e3740286086eef519d
msmanawadu <msmanawadu@gmail.com>
2023-06-25 13:53:42 +0530

    Protecting routing endpoints with authenticated API requests

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import { db, connectToDb } from './db.js';
 
 // Load Firebase Private keys
-const credentials = JSON.parse(fs.readFileSync('../credentials.json'));
+const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
 
 // Setup Firebase Admin for the Firebase project
 admin.initializeApp({
@@ -14,28 +14,64 @@ admin.initializeApp({
 const app = express();
 app.use(express.json());
 
+// Verify whether the request is authenticated
+app.use(async (req, res, next) => {
+  // Retrieve the auth token in request headers
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    try {
+      // Load the firebase user for the auth token
+      const user = await admin.auth().verifyIdToken(authtoken);
+
+      // Attach the logged-in user to the request
+      req.user = user;
+    } catch (e) {
+      return res.sendStatus(400);
+    }
+  }
+  // Default value firebase user
+  req.user = req.user || {};
+  return next();
+});
+
 // Get an article
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
 
+  // Firebase user id
+  const { uid } = req.user;
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
+    // Has the user already upvoted this article ?
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.includes(uid);
     res.json(article);
   } else {
     res.sendStatus(404);
   }
 });
 
+// Verify whether the request is authenticated
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+});
+
 // Create a comment for an article
 app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
-  const { postedBy, text } = req.body;
+  const { text } = req.body;
+  const { email } = req.user;
 
   await db.collection('articles').updateOne(
     { name },
     {
-      $push: { comments: { postedBy, text } },
+      $push: { comments: { postedBy: email, text } },
     }
   );
 
@@ -52,19 +88,31 @@ app.post('/api/articles/:name/comments', async (req, res) => {
 // Upvote an article
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params;
+  // Firebase user id
+  const { uid } = req.user;
 
-  await db.collection('articles').updateOne(
-    { name },
-    {
-      $inc: { upvotes: 1 },
-    }
-  );
-
-  // Load the upvoted article
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    res.json(article);
+    // Has the user already upvoted this article ?
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.includes(uid);
+
+    if (canUpvote) {
+      await db.collection('articles').updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          // Declare that user has already upvoted once
+          $push: { upvoteIds: uid },
+        }
+      );
+    }
+
+    // Load the upvoted article
+    const UpdatedArticle = await db.collection('articles').findOne({ name });
+
+    res.json(UpdatedArticle);
   } else {
     res.send('The article does not exist');
   }
